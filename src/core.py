eeb0c2cb2d27b473af10b8c6550362bd265afb4d
Eli <mcditto99@gmail.com>
2023-02-09 17:06:57 -0500

    python glob, working camera input

--- src/core.py
+++ src/core.py
@@ -1,19 +1,55 @@
-# from retinaface import RetinaFace
-# from deepface import DeepFace
-# import matplotlib.pyplot as plt
-# import os
+from retinaface import RetinaFace
+import matplotlib.pyplot as plt
+from glob import glob
+import os
 
-# img_path = os.getcwd() + "/testImages/test0.jpg"
-# faces = RetinaFace.extract_faces(img_path=img_path, align=True)
+autisticImages = glob("autismDataset/test/autistic/*.jpg")
+nonAutisticImages = glob("autismDataset/test/non_autistic/*.jpg")
 
-# for face in faces:
-#   plt.imshow(face)
-#   plt.show()
+total = 0
+success = 0
+
+for img_path in autisticImages:
+	faces = RetinaFace.extract_faces(img_path=img_path, align=True)
+	if len(faces) != 0:
+		success += 1
+		print(f"Found face in {img_path}")
+		for face in faces:
+			plt.imshow(face)
+			plt.show()
+	else:
+		print(f"No face in {img_path}")
+
+	total += 1
+
+print("-----------------------------")
+print(f"Found faces in {success}/{total} images of autistic children")
+print("-----------------------------")
+
+total = 0
+success = 0
+
+for img_path in nonAutisticImages:
+	faces = RetinaFace.extract_faces(img_path=img_path, align=True)
+	if len(faces) != 0:
+		success += 1
+		print(f"Found face in {img_path}")
+		for face in faces:
+			plt.imshow(face)
+			plt.show()
+	else:
+		print(f"No face in {img_path}")
+		
+	total += 1
+print("-----------------------------")
+print(f"Found faces in {success}/{total} images of non_autistic children")
+print("-----------------------------")
 
 # https://pypi.org/project/retina-face/
 # https://github.com/serengil/deepface
 # Other option: https://machinelearningmastery.com/how-to-perform-face-detection-with-classical-and-deep-learning-methods-in-python-with-keras/
 
+'''
 import pyrealsense2.pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -48,3 +84,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+'''
