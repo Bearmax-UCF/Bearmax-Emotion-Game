9dfb57ef094f912707f5afc9fcb68d67105048d8
Elijah Smith <mcditto99@gmail.com>
2023-02-15 19:03:43 -0500

    added submodule

--- src/core.py
+++ src/core.py
@@ -4,71 +4,61 @@ Extracts faces and emotions from video capture
 
 import cv2
 
-class EmotionGame:
-    '''
-    Required Data:
-    Correct: Array
-    Wrong: Array
-    TotalPlays: int
-    NumCorrect: int
-    LastPlayed: TimeStamp
-    UserID: String
-    
-    Need clarification with Rachel before we use this, which seems to mix individual 
-    and overall stats into each table record
-    '''
+'''
+Required Data:
+Correct: Array
+Wrong: Array
+TotalPlays: int
+NumCorrect: int
+LastPlayed: TimeStamp
+UserID: String
 
-    def __init__(self, video):
-        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        self.capture = cv2.VideoCapture(0)
+Need clarification with Rachel before we use this, which seems to mix individual 
+and overall stats into each table record
+'''
 
-    def run(self): # TODO: needs to run without blocking
-        while True: # TODO: Better condition
-            ret, img = capture.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+def pushRunStats(): # TODO: send current data to API and clear stats
+    return
 
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                roi_color = img[y:y+h, x:x+w]
-                cv2.imshow("Face", roi_color)
-                
-            cv2.imshow('img', img)
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:
-                break
+def invalidCamera(capture):
+    return capture is None or not capture.isOpened()
 
-    def stop(self):
-        # TODO: stop the non-blocking process and push stats to API
-        return
+def run():
+    # Need a better face recognizer, this is only frontal which isn't good enough when you are looking not directly at camera
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    capture = None
 
-    def pushRunStats(self): # TODO: send current data to API and clear stats
-        return
+    for i in range(0, 5):
+        capture = cv2.VideoCapture(i)
 
-    def __del__(self):
-        # TODO: Send data to API
+        if invalidCamera(capture):
+            continue
+
+        print(f"Found webcam at index {i}")
+        break
+
+    if invalidCamera(capture):
+        raise Exception("Could not find webcam to use for video capture")
+
+    while True: # TODO: Better condition, run without blocking
+        ret, img = capture.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_color = img[y:y+h, x:x+w]
+            cv2.imshow("Face", roi_color)
+            
+        cv2.imshow("Full", img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+        
+    pushRunStats()
+    if capture != None:
         capture.release()
-        cv2.destroyAllWindows()
-
-def createGameInstance(video=0):
-    gameInstance = EmotionGame(video)
-
-def startGame(gameInstance):
-    if not __isGameInstance(gameInstance):
-        return
-    gameInstance.run()
-
-def terminateGame(gameInstance):
-    if not __isGameInstance(gameInstance):
-        return
-    
-
-def __isGameInstance(gameInstance):
-    if gameInstance == None or not type(gameInstance) is EmotionGame:
-        print("Error: provided parameter is not of type EmotionGame")
-        return False
-    return True
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    instance = createGameInstance()
-    startGame(instance)
+    run()
