2d7369d5c5b15a7ce6dc8880cde98290e221a94e
ElijahMSmith <mcditto99@gmail.com>
2023-02-28 12:39:41 -0500

    More data collection

--- src/stats.py
+++ src/stats.py
@@ -1,18 +1,21 @@
 from glob import glob
 
-types = ["happy", "angry", "sad", "surprised", "disgusted", "neutral"]
+types = ["happy", "angry", "sad", "surprise", "neutral", "other"]
 
-print("\n\nEmotion\t\tTest\tTrain\tTotal")
+print("\n\nEmotion\t\tTest\tTrain\tUncropped\tTotal")
 
 for emotion in types:
     testImages = glob(f"testing/{emotion}/*.jpg")
     trainingImages = glob(f"training/{emotion}/*.jpg")
+    uncroppedImages = glob(f"uncropped/{emotion}/*.jpg")
 
     testc = len(testImages)
     trainingc = len(trainingImages)
+    uncroppedc = len(uncroppedImages)
 
     tabs = "\t\t" if len(emotion) <= 7 else "\t"
 
-    print(emotion + tabs + str(testc) + " \t" + str(trainingc) + " \t" + str(testc + trainingc))
+    print(emotion + tabs + str(testc) + "\t" +
+          str(trainingc) + "\t" + str(uncroppedc) + "\t\t" + str(testc + trainingc + uncroppedc))
 
 print("\n\n")
