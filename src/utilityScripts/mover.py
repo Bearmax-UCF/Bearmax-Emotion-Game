a19b340e35938bfb6d9ef21dc97d7a83be20fed3
ElijahMSmith <mcditto99@gmail.com>
2023-03-18 14:55:37 -0400

    mover script update

--- src/utilityScripts/mover.py
+++ src/utilityScripts/mover.py
@@ -1,10 +1,32 @@
 import shutil
 from glob import glob
+import cv2
+import os
 
-emotion = "neutral"
-allImages = glob("testing/{emotion}/*.jpg".format(emotion=emotion))
+emotions = ["happy", "angry", "sad", "neutral", "other"]
+dir = "training"
+emotion = "other"
 
-for image in allImages:
-    newName = "t" + image.split("/")[2]
-    # print(image + " -> " + 'training/{emotion}/{newName}'.format(emotion=emotion, newName=newName))
-    shutil.move(image, 'training/{emotion}/{newName}'.format(emotion=emotion, newName=newName))
+images = glob(f"{dir}/{emotion}/*.jpg")
+count = 0
+
+for img_path in images:
+    image = cv2.imread(img_path)
+    cv2.imshow('Safe?', image)
+    print(img_path)
+    print("1 - keep, 2 - move to manual")
+    pressed = cv2.waitKey(0)
+
+    parts = img_path.split("/")
+    img_name = parts[len(parts) - 1]
+
+    if pressed == 49: # 1
+        print(f"verified/{emotion}/{img_name}")
+        # os.replace(img_path, f"verified/{emotion}/{img_name}")
+    elif pressed == 50:
+        print(f"manual/{emotion}/{img_name}")
+        # os.replace(img_path, f"manual/{emotion}/{img_name}")
+    
+    count+=1
+    if count == 100:
+        break
