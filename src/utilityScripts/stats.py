d4de412e84e7430adec8f990354f34df373580c4
Elijah Smith <mcditto99@gmail.com>
2023-03-22 15:44:55 -0400

    Finally moved last images accidentally in the wrong folder

--- src/utilityScripts/stats.py
+++ src/utilityScripts/stats.py
@@ -1,21 +1,18 @@
 from glob import glob
 
-types = ["happy", "angry", "sad", "neutral", "other"]
+types = ["happy", "angry", "sad", "neutral", "other", "invalid"]
 
-print("\n\nEmotion\t\tTest\tTrain\tUncropped\tTotal")
+print("\n\nEmotion\t\tVerified\tUncropped")
 
 for emotion in types:
-    testImages = glob(f"testing/{emotion}/*.jpg")
-    trainingImages = glob(f"training/{emotion}/*.jpg")
-    uncroppedImages = glob(f"uncropped/{emotion}/*.jpg")
+    verified = glob(f"verified/{emotion}/*.jpg")
+    uncropped = glob(f"uncropped/{emotion}/*.jpg")
 
-    testc = len(testImages)
-    trainingc = len(trainingImages)
-    uncroppedc = len(uncroppedImages)
+    lver = len(verified)
+    luc = len(uncropped)
 
     tabs = "\t\t" if len(emotion) <= 7 else "\t"
 
-    print(emotion + tabs + str(testc) + "\t" +
-          str(trainingc) + "\t" + str(uncroppedc) + "\t\t" + str(testc + trainingc + uncroppedc))
+    print(emotion + tabs + str(lver) + "\t\t" + str(luc))
 
 print("\n\n")
